7415813281fbcf76988741398cdc490ce326eb68
Jason <jasonmgill@gmail.com>
2021-10-14 17:55:52 -0700

    React native tutorial

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	View,
+	ImageBackground,
+	// Image,
+	// TouchableWithoutFeedback,
+	// TouchableOpacity,
+	// TouchableHighlight,
+	// TouchableNativeFeedback,
+	// Button,
+	// Alert,
+	// Platform,
+	// SafeAreaView,
+	// StatusBar,
+} from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+// import {
+// 	useDimensions,
+// 	useDeviceOrientation,
+// } from '@react-native-community/hooks';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+	},
+	image: {
+		flex: 1,
+		justifyContent: 'center',
+	},
+	text: {
+		color: 'white',
+		fontSize: 42,
+		lineHeight: 84,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		backgroundColor: '#000000c0',
+	},
 });
